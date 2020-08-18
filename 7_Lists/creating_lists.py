a5e0ba101b8fe94d731f44e9f7072050ccf948b3
racheldmiller <rachelmiller415@gmail.com>
2020-08-17 21:42:12 -0700

    list exercise

---
+++ 7_Lists/creating_lists.py
@@ -0,0 +1,8 @@
+# Create 2 separate lists.
+# 1st list: `my_stuff`. Must be at least 4 elements long and must contain at least 1 string & 1 float.
+# 2nd list: `nums`. List containing all numbers between 1 and 99 (including 99).
+
+# Define my_stuff
+my_stuff = ["Ryan", 6.999, 3, True]
+# Define nums
+nums = list(range(1, 100))
